c345ba4f5ddfefbbc2220c81b8c975a17e4ea1aa
Elena Ivanova <elena_ivanova@epam.com>
2022-11-26 15:56:16 +0300

    Added basic linters and settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 
-function App() {
+import { store } from "./app/store";
+
+import styles from "./App.module.scss";
+
+function App(): React.ReactElement {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Provider store={store}>
+        <div className={styles.app}></div>
+      </Provider>
+    </BrowserRouter>
   );
 }
 
